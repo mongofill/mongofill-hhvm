8b159f7c0208bbfad18964dc60fef745c877cd24
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-03-19 20:23:59 +0100

    poc

--- config.cmake
+++ config.cmake
@@ -1,2 +1,19 @@
+FIND_PATH(BSON_INCLUDE_DIR NAMES bson.h
+    PATHS /usr/include /usr/include/libbson-1.0 /usr/local/include /usr/local/include/libbson-1.0)
+
+FIND_LIBRARY(BSON_LIBRARY NAMES bson-1.0 PATHS /lib /usr/lib /usr/local/lib)
+
+IF (BSON_INCLUDE_DIR AND BSON_LIBRARY)
+    MESSAGE(STATUS "bson Include dir: ${BSON_INCLUDE_DIR}")
+    MESSAGE(STATUS "libbson library: ${BSON_LIBRARY}")
+ELSE()
+    MESSAGE(FATAL_ERROR "Cannot find libbson library")
+ENDIF()
+
+include_directories(${BSON_INCLUDE_DIR})
+
 HHVM_EXTENSION(bson src/bson.cpp)
 HHVM_SYSTEMLIB(bson src/bson.php)
+
+target_link_libraries(bson ${BSON_LIBRARY})
+
--- src/bson.cpp
+++ src/bson.cpp
@@ -1,16 +1,35 @@
 #include "hphp/runtime/base/base-includes.h"
+#include <bson.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 namespace HPHP {
 const StaticString s_Mongo("Mongo");
 
 //////////////////////////////////////////////////////////////////////////////
 // functions
 
-static Array HHVM_FUNCTION(bson_decode, const String& bson) {
+static Array HHVM_FUNCTION(bson_decode, const Variant& anything) {
   throw NotImplementedException("bson_decode");
 }
 
-static String HHVM_FUNCTION(bson_encode, const Variant& anything) {
-  throw NotImplementedException("bson_encode");
+static String HHVM_FUNCTION(bson_encode, const String& test) {
+	bson_t bson, foo, bar, baz;
+	bson_init(&bson);
+        
+	bson_append_document_begin(&bson, "foo", -1, &foo);
+        bson_append_document_begin(&foo, "bar", -1, &bar);
+        bson_append_array_begin(&bar, "baz", -1, &baz);
+        bson_append_int32(&baz, "0", -1, 1);
+        bson_append_int32(&baz, "1", -1, 2);
+        bson_append_int32(&baz, "2", -1, 3);
+        bson_append_array_end(&bar, &baz);
+        bson_append_document_end(&foo, &bar);
+        bson_append_document_end(&bson, &foo);
+
+	const char* output = (const char*) bson_get_data(&bson);
+				
+	return String(output, bson.len, CopyString);
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -26,7 +45,7 @@ class mongoExtension : public Extension {
 } s_mongo_extension;
 
 // Uncomment for non-bundled module
-//HHVM_GET_MODULE(mongo);
+HHVM_GET_MODULE(mongo);
 
 //////////////////////////////////////////////////////////////////////////////
 } // namespace HPHP
