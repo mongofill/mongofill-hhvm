19b9c6cbcc6336e813cae8b26009463a9921ed1f
Máximo Cuadros <mcuadros@gmail.com>
2014-04-12 18:49:18 +0200

    merge fix

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 : ${HPHP_HOME?"HPHP_HOME environment variable must be set!"}
 
 git submodule init
-hhvm tools/merge.php
+$HPHP_HOME/hphp/hhvm/hhvm tools/merge.php
 $HPHP_HOME/hphp/tools/hphpize/hphpize
 cmake .
 make
